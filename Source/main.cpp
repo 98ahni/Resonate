a1095f9197e439d45f4926829c1e3a852fb355a7
98ahni <98ahni@gmail.com>
2024-01-23 22:53:45 +0000

    emcc

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <emmintrin.h>
+#include <emscripten.h>
+
+void loop(void* window){
+
+}
 
 int main(){
-
+    emscripten_set_main_loop_arg(loop, (void*)0, 0, false);
     return 0;
 }
