6ffdeebf948c53a0a8acb2ac1dc38cb0be69bdc0
98ahni <60541902+98ahni@users.noreply.github.com>
2024-01-24 18:12:35 +0000

    Actually made compilation work

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <emscripten.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 void loop(void* window){
+    ImGui::Begin("Test");
 
 }
 
 int main(){
-    emscripten_set_main_loop_arg(loop, (void*)0, 0, false);
+    GLFWwindow* window;
+    glfwCreateWindow(1600, 900, "Resonate", 0, 0);
+    ImGui_ImplGlfw_InitForOpenGL(window,true);
+    emscripten_set_main_loop_arg(loop, (void*)window, 0, false);
     return 0;
 }
