771466283ff3976f6186d40d2d8ae37b7877223a
98ahni <98ahni@gmail.com>
2024-03-20 14:51:32 +0000

    Basic layout for the touch controls.

---
+++ Source/Windows/TouchControl.cpp
@@ -0,0 +1,35 @@
+#include "TouchControl.h"
+
+void TouchControl::OnImGuiDraw()
+{
+    if(ImGui::Checkbox("Edit Mode", &myIsCharMode))
+    {
+    }
+    if(ImGui::Button("Back 5s"))
+    {
+    }
+    if(ImGui::Button("Skip 5s"))
+    {
+    }
+    if(ImGui::Button("Up"))
+    {
+    }
+    if(ImGui::Button("Down"))
+    {
+    }
+    if(ImGui::Button("Left"))
+    {
+    }
+    if(ImGui::Button("Right"))
+    {
+    }
+    if(ImGui::Button("Add/Remove"))
+    {
+    }
+    if(ImGui::Button("Time"))
+    {
+    }
+    if(ImGui::Button("End"))
+    {
+    }
+}
