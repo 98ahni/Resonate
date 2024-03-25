6d3cf93193decbff04be14909ae715d541db06dc
98ahni <60541902+98ahni@users.noreply.github.com>
2024-03-25 08:09:11 +0000

    Fixed icons for TouchControl.

--- Source/Windows/TouchControl.cpp
+++ Source/Windows/TouchControl.cpp
@@ -58,7 +58,7 @@ void TouchControl::OnImGuiDraw()
     }
     // Back Arrow
     drawList->PathArcTo(contentSize * backPosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos(), buttonSize.x * .3f, -1.57f, 3.54f);
-    drawList->PathStroke(IM_COL32_WHITE, 0, buttonSize.x * .03f);
+    drawList->PathStroke(IM_COL32_WHITE, 0, buttonSize.x * .05f);
     ImVec2 triangleTip = ImVec2(contentSize * backPosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos());
     triangleTip.y -= buttonSize.x * .3f;
     triangleTip.x -= buttonSize.x * .1f;
@@ -71,7 +71,7 @@ void TouchControl::OnImGuiDraw()
     }
     // Skip Arrow
     drawList->PathArcTo(contentSize * skipPosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos(), buttonSize.x * .3f, 4.71f, .4f);
-    drawList->PathStroke(IM_COL32_WHITE, 0, buttonSize.x * .03f);
+    drawList->PathStroke(IM_COL32_WHITE, 0, buttonSize.x * .05f);
     triangleTip = ImVec2(contentSize * skipPosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos());
     triangleTip.y -= buttonSize.x * .3f;
     triangleTip.x += buttonSize.x * .1f;
@@ -118,24 +118,46 @@ void TouchControl::OnImGuiDraw()
     drawList->AddTriangleFilled(triangleTip, ImVec2(triangleTip.x - buttonSize.x * .6f, triangleTip.y - buttonSize.x * .3f), ImVec2(triangleTip.x - buttonSize.x * .6f, triangleTip.y + buttonSize.x * .3f), IM_COL32_WHITE);
     // ~Right Arrow
     ImGui::SetCursorPos(contentSize * togglePosMult + offset);
-    if(ImGui::Button("Add/Remove", buttonSize))
+    if(ImGui::Button("##Add/Remove", buttonSize))
     {
         myEditor->ToggleTokenHasTime();
     }
+    // Toggle Token Icon
+    ImVec2 linePos = ImVec2(contentSize * togglePosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos());
+    drawList->PathLineTo({linePos.x - buttonSize.x * .15f, linePos.y - buttonSize.x * .3f});
+    drawList->PathLineTo({linePos.x - buttonSize.x * .3f, linePos.y - buttonSize.x * .3f});
+    drawList->PathLineTo({linePos.x - buttonSize.x * .3f, linePos.y + buttonSize.x * .3f});
+    drawList->PathLineTo({linePos.x - buttonSize.x * .15f, linePos.y + buttonSize.x * .3f});
+    drawList->PathStroke(IM_COL32_WHITE, 0, buttonSize.x * .05f);
+    drawList->PathLineTo({linePos.x + buttonSize.x * .15f, linePos.y - buttonSize.x * .3f});
+    drawList->PathLineTo({linePos.x + buttonSize.x * .3f, linePos.y - buttonSize.x * .3f});
+    drawList->PathLineTo({linePos.x + buttonSize.x * .3f, linePos.y + buttonSize.x * .3f});
+    drawList->PathLineTo({linePos.x + buttonSize.x * .15f, linePos.y + buttonSize.x * .3f});
+    drawList->PathStroke(IM_COL32_WHITE, 0, buttonSize.x * .05f);
+    drawList->AddCircleFilled({linePos.x, linePos.y - buttonSize.x * .15f}, buttonSize.x * .07f, IM_COL32_WHITE);
+    drawList->AddCircleFilled({linePos.x, linePos.y + buttonSize.x * .2f}, buttonSize.x * .07f, IM_COL32_WHITE);
+    // ~Togle Token Icon
     ImGui::SetCursorPos(contentSize * timePosMult + offset);
-    if(ImGui::Button("Time", buttonSize))
+    if(ImGui::Button("##Time", buttonSize))
     {
         myEditor->RecordStartTime();
     }
     // Time Arrow
-    triangleTip = ImVec2(contentSize * rightPosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos());
+    triangleTip = ImVec2(contentSize * timePosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos());
     triangleTip.x += buttonSize.x * .3f;
-    drawList->AddTriangleFilled(triangleTip, ImVec2(triangleTip.x - buttonSize.x * .6f, triangleTip.y - buttonSize.x * .3f), ImVec2(triangleTip.x - buttonSize.x * .6f, triangleTip.y + buttonSize.x * .3f), IM_COL32_WHITE);
+    triangleTip.y -= buttonSize.x * .3f;
+    drawList->AddTriangleFilled(triangleTip, ImVec2(triangleTip.x - buttonSize.x * .6f, triangleTip.y + buttonSize.x * .6f), ImVec2(triangleTip.x, triangleTip.y + buttonSize.x * .6f), IM_COL32_WHITE);
     // ~Time Arrow
     ImGui::SetCursorPos(contentSize * endPosMult + offset);
-    if(ImGui::Button("End", buttonSize))
+    if(ImGui::Button("##End", buttonSize))
     {
         myEditor->RecordEndTime();
     }
+    // End Arrow
+    triangleTip = ImVec2(contentSize * endPosMult + offset + (buttonSize * .5f) + ImGui::GetWindowPos());
+    triangleTip.x -= buttonSize.x * .3f;
+    triangleTip.y -= buttonSize.x * .3f;
+    drawList->AddTriangleFilled(triangleTip, ImVec2(triangleTip.x, triangleTip.y + buttonSize.x * .6f), ImVec2(triangleTip.x + buttonSize.x * .6f, triangleTip.y + buttonSize.x * .6f), IM_COL32_WHITE);
+    // ~End Arrow
     Gui_End();
 }
