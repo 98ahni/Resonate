860027e3965bf5f7ebe341d7df9e076c666d79f8
98ahni <98ahni@gmail.com>
2024-04-18 18:04:52 +0200

    Fixed memory issues while threadding audio.

--- bin/public/plugins/audiostretchworker.js
+++ bin/public/plugins/audiostretchworker.js
@@ -5,11 +5,14 @@ onmessage = async function (msg)
 {
     const channelDataArray = msg.data[0];
     const samples = msg.data[1];
-    const ind = msg.data[2];
-    var outblob = null;
-    //if(ind == 5) outblob = Module.audioBufferToBlob(Module.stretch(audioEmptyBuff, audioBuffer, 2));
-    //else
-    outblob = audioBufferToBlob(stretch(channelDataArray, samples, 1 / (ind * 0.1)));
-    console.log('Streched blob nr ' + ind);
-    postMessage([outblob, ind]);
+    //const ind = msg.data[2];
+    for(var ind = 9; ind > 0; ind--){
+        var outblob = null;
+        //if(ind == 5) outblob = Module.audioBufferToBlob(Module.stretch(audioEmptyBuff, audioBuffer, 2));
+        //else
+        outblob = Module.audioDataArrayToBlob(Module.stretch(channelDataArray, samples, 1 / (ind * 0.1)));
+        console.log('Streched blob nr ' + ind);
+        postMessage([outblob, ind]);
+    }
+    //outblob = undefined;      // Memory management
 }
