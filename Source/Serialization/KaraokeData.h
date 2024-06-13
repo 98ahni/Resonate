52d49064ffac7e26b67fefd0f078d89c82c96f9e
98ahni <98ahni@gmail.com>
2024-06-13 14:58:36 +0200

    Just licence and legal stuff.

--- Source/Serialization/KaraokeData.h
+++ Source/Serialization/KaraokeData.h
@@ -1,3 +1,6 @@
+//  This file is licenced under the GNU General Public License and the Resonate Supplemental Terms. (See file LICENSE and LICENSE-SUPPLEMENT or <https://github.com/98ahni/Resonate>)
+//  <Copyright (C) 2024 98ahni> Original file author
+
 #include <string>
 #include <vector>
 #include <unordered_map>
