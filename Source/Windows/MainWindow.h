0733e22813b67d08e9a9ee0cc58412805120324f
Niklas-EODynamics <98ahni@gmail.com>
2024-09-09 16:59:42 +0200

    Fixed copy/paste. Again.

--- Source/Windows/MainWindow.h
+++ Source/Windows/MainWindow.h
@@ -8,6 +8,8 @@
 
 // Global WebGPU required states
 struct ImFont;
+enum MainWindow_Platform { MainWindow_Unspecified = 0, MainWindow_Windows = 1<<0, MainWindow_Mac = 1<<1, MainWindow_iOS = 1<<2, MainWindow_Android = 1<<3, MainWindow_Linux = 1<<4, 
+MainWindow_Apple = MainWindow_Mac | MainWindow_iOS};
 struct MainWindow
 {
 	static inline WGPUDevice        Device = nullptr;
@@ -19,16 +21,18 @@ struct MainWindow
 	static inline bool				HasWebGPU = true;
 	
 	inline static double			DPIScale = 1;
-	static inline ImFont*           Font = nullptr;
-	inline static std::string		Name = "";
-	static inline std::string		IconPath = "";
+	static inline MainWindow_Platform RuntimePlatform;
+	inline static ImFont*           Font = nullptr;
+	static inline std::string		Name = "";
+	inline static std::string		IconPath = "";
 
-	inline static ImVec2			DockSizeOffset = {0, 0};		// Space to reserve in the x/y axis for other content
+	static inline ImVec2			DockSizeOffset = {0, 0};		// Space to reserve in the x/y axis for other content
 };
 
 void MainWindow_Init(const char* name, void** outWindow);
 void MainWindow_NewFrame(void* window);
 void MainWindow_RenderFrame();
+bool MainWindow_IsPlatform(MainWindow_Platform platform);
 void MainWindow_SetName(std::string name);
 void MainWindow_SetIcon(std::string iconName);
 void MainWindow_StyleVarsShadow(struct ImGuiStyle* dst = nullptr);
