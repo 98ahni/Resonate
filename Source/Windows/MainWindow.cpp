43c850668c56cd8ba101b51878f19fe220918a7d
98ahni <98ahni@gmail.com>
2024-10-28 14:39:07 +0100

    Disabled imgui keyboard nav (Alt) to allow for keyboard shortcuts.

--- Source/Windows/MainWindow.cpp
+++ Source/Windows/MainWindow.cpp
@@ -251,7 +251,7 @@ void MainWindow_Init(const char* name, void** outWindow)
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;		// Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;		// Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;		// Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;			// Enable Docking
 	io.ConfigMacOSXBehaviors = MainWindow_IsPlatform(MainWindow_Apple);
