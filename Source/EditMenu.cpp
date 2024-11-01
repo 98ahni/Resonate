fc47cedea5a62421e9aa813f023979b05868cad9
98ahni <98ahni@gmail.com>
2024-11-01 17:03:36 +0100

    Added Copyright notice.

--- Source/EditMenu.cpp
+++ Source/EditMenu.cpp
@@ -1,3 +1,6 @@
+//  This file is licenced under the GNU Affero General Public License and the Resonate Supplemental Terms. (See file LICENSE and LICENSE-SUPPLEMENT or <https://github.com/98ahni/Resonate>)
+//  <Copyright (C) 2024 98ahni> Original file author
+
 #include "EditMenu.h"
 #include <imgui/imgui.h>
 #include "Windows/TimingEditor.h"
--- Source/EditMenu.h
+++ Source/EditMenu.h
@@ -1,4 +1,5 @@
-
+//  This file is licenced under the GNU Affero General Public License and the Resonate Supplemental Terms. (See file LICENSE and LICENSE-SUPPLEMENT or <https://github.com/98ahni/Resonate>)
+//  <Copyright (C) 2024 98ahni> Original file author
 
 namespace Menu
 {
